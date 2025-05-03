961450bc7b92267ef974be60087d038e31054788
Mario Mairovitz <62700212+mairovitzmario@users.noreply.github.com>
2025-05-03 21:57:23 +0300

    added cuda

--- research/model_class.py
+++ research/model_class.py
@@ -9,26 +9,21 @@ class LSTMModel(nn.Module):
     def __init__(self, input_size=1, hidden_layer_size=50, output_size=1):
         super(LSTMModel, self).__init__()
 
-        # Define the LSTM layer
         self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
-
-        # Define a dropout layer to prevent overfitting
         self.dropout = nn.Dropout(0.2)
 
-        # Define the fully connected layer
-        self.fc = nn.Linear(hidden_layer_size, output_size)
+        # Added intermediate dense layer with 3 nodes
+        self.hidden = nn.Linear(hidden_layer_size, 3)
+        self.relu = nn.ReLU()
+
+        self.fc = nn.Linear(3, output_size)
 
     def forward(self, x):
-        # Pass through the LSTM layer
-        lstm_out, (hn, cn) = self.lstm(x)
-
-        # Dropout layer for regularization
+        lstm_out, _ = self.lstm(x)
         lstm_out = self.dropout(lstm_out)
-
-        # Take the output of the last time step
         last_time_step = lstm_out[:, -1, :]
 
-        # Pass the output through the fully connected layer
-        output = self.fc(last_time_step)
+        hidden_out = self.relu(self.hidden(last_time_step))
+        output = self.fc(hidden_out)
 
         return output
