161209a71fe825c6510786bc8e7d8917e9325dd6
Mario Mairovitz <62700212+mairovitzmario@users.noreply.github.com>
2025-05-08 17:34:16 +0300

    benchmarked LSTM model with added noise based on the returns from the lookback period. got better results than all the other methods

--- research/model_class.py
+++ research/model_class.py
@@ -6,24 +6,27 @@ class LSTMModel(nn.Module):
 
     LOOKBACK = 30
 
-    def __init__(self, input_size=1, hidden_layer_size=50, output_size=1):
+    def __init__(
+        self, input_size=1, hidden_layer_size=50, intermediate_size=10, output_size=1
+    ):
         super(LSTMModel, self).__init__()
 
         self.lstm = nn.LSTM(input_size, hidden_layer_size, batch_first=True)
         self.dropout = nn.Dropout(0.2)
 
-        # Added intermediate dense layer with 3 nodes
-        self.hidden = nn.Linear(hidden_layer_size, 3)
-        self.relu = nn.ReLU()
+        # First dense layer after LSTM
+        self.fc1 = nn.Linear(hidden_layer_size, intermediate_size)
+        self.relu = nn.LeakyReLU()
 
-        self.fc = nn.Linear(3, output_size)
+        # Second dense layer for final output
+        self.fc2 = nn.Linear(intermediate_size, output_size)
 
     def forward(self, x):
         lstm_out, _ = self.lstm(x)
         lstm_out = self.dropout(lstm_out)
         last_time_step = lstm_out[:, -1, :]
 
-        hidden_out = self.relu(self.hidden(last_time_step))
-        output = self.fc(hidden_out)
+        hidden_out = self.relu(self.fc1(last_time_step))
+        output = self.fc2(hidden_out)
 
         return output
